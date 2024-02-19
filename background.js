a66069671e09f90cd121981ad7c84161268745bf
Moriarity <CMoriarity@ppg.com>
2024-02-19 09:14:05 -0500

    more modularity

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(tab.id, {file: "content.js"}, function() {
+  chrome.tabs.executeScript(tab.id, {file: "content_updated.js"}, function() {
     // Handle completion or errors if needed
     console.log('Content script executed in the current tab');
   });
