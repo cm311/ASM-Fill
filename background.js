31956717494735cfb53b040b7dc08a3c36814ba9
cm311 <58615389+cm311@users.noreply.github.com>
2024-10-20 06:39:43 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 console.log("Hello");
-=======
+
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(tab.id, {file: "content_updated.js"}, function() {
     // Handle completion or errors if needed
     console.log('Content script executed in the current tab');
   });
 });
->>>>>>> 676d64ef0bcb3bced749b7ba10dffb844db3504c
